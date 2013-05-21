86a059519c53c69755c619c3ad3b4dfb81d49869
razic <razic@viralkitty.com>
2013-05-21 14:01:35 +0700

    add base skeleton

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Dependencies
+ */
+var Emitter = require('emitter');
+
+/**
+ * Expose
+ */
+module.exports = functionSignatures;
+
+/**
+ * Constructor
+ */
+function functionSignatures() {
+};
+
+/**
+ * Prototype
+ */
+functionSignatures.prototype = {
+  normalize: function() {
+  }
+};
+
+/**
+ * Mixin
+ */
+Emitter(functionSignatures.prototype);
