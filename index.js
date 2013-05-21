c1aa1fae15c87476e5b0b8ed8fde7c3ab80ae355
razic <razic@viralkitty.com>
2013-05-21 15:39:57 +0700

    initial implementation 99%

--- index.js
+++ index.js
@@ -11,18 +11,24 @@ module.exports = functionSignatures;
 /**
  * Constructor
  */
-function functionSignatures() {
+function functionSignatures(signatures) {
+  this._signatures = signatures;
 };
 
 /**
  * Prototype
  */
-functionSignatures.prototype = {
-  normalize: function() {
-  }
+functionSignatures.prototype.normalize = function() {
+  arguments = Array.prototype.slice.call(arguments, 0);
+
+  for (var signature in this._signatures)
+    if (this._signatures.hasOwnProperty(signature))
+      if (this._signatures[signature].apply(this, arguments))
+        return this.emit.apply(this, [signature].concat(arguments));
 };
 
 /**
  * Mixin
  */
 Emitter(functionSignatures.prototype);
+
--- test/function-signatures.js
+++ test/function-signatures.js
@@ -1,15 +1,62 @@
 var assert = require('assert'),
     functionSignatures = require('function-signatures'),
-    prototype = functionSignatures.prototype;
+    prototype = functionSignatures.prototype,
+    object = {
+      "one object": function() { return arguments.length === 1; },
+      "duplicate of above": function() { return arguments.length === 1; },
+      "two objects": function() { return arguments.length === 2; }
+    },
+    signatures = new functionSignatures(object);
 
 describe('functionSignatures', function() {
   it('should be a function', function() {
     assert(typeof functionSignatures === 'function');
   });
 
-  describe('the prototype', function() {
-    it('should have a `normalize` function', function() {
-      assert(typeof prototype.normalize === 'function');
+  describe('new functionSignatures(object)', function() {
+    it('should set `_signatures` property to be the object passed', function() {
+      assert(signatures._signatures === object);
+    });
+
+    describe('.normalize(arguments)', function() {
+      it('should only emit the first signature that returns true', function() {
+        var emits = [];
+
+        signatures.on('one object', function() {
+          emits.unshift('one object');
+        });
+
+        signatures.on('duplicate of above', function() {
+          emits.unshift('duplicate of above');
+        });
+
+        signatures.normalize({});
+        signatures.off();
+
+        assert(emits.length === 1);
+        assert(emits[0] === "one object");
+      });
+
+      it('should pass arguments to the event handler properly', function() {
+        var args;
+
+        signatures.on('two objects', function() { args = arguments; });
+        signatures.normalize('foo', 'bar');
+        signatures.off();
+
+        assert(args[0] === 'foo');
+        assert(args[1] === 'bar');
+      });
+
+      it('should change the context of the event handler', function() {
+        var context;
+
+        signatures.on('two objects', function() { context = this; });
+        signatures.normalize('foo', 'bar');
+        signatures.off();
+
+        assert(context === signatures);
+      });
     });
   });
 });
